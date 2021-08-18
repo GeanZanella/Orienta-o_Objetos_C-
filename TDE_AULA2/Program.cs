4c49b604a263a7f708f33aea79c4d6a09df7e1de
GeanZanella <geanzanella@rede.ulbra.br>
2021-08-17 22:31:08 -0300

    tde_2

---
+++ TDE_AULA2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TDE_AULA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autor autorLivro = new Autor("João do Caminhão","032 902 034 03","joao_caminhao@gmail.com");
+            Livro nomeLivro = new Livro("Pelo Mundo","Livro Aventura",20.00,"01-0002-003",autorLivro);
+
+            Console.WriteLine("teste "+nomeLivro.getNome());
+        }
+
+    }
+}
